8b74918bcf490ec871e1726a0420439935a5c9a9
Gegel85 <Gegel85@laposte.net>
2019-01-23 00:09:53 +0100

    Added example files

---
+++ examples/json_parser.c
@@ -0,0 +1,64 @@
+#include <JsonParser.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+bool	strIsNbr(char *nbr)
+{
+	int i = 0;
+
+	for (; nbr[i] == '+' || nbr[i] == '-'; i++);
+	for (; isdigit(nbr[i]); i++);
+	return (nbr[i] == 0 && i != 0 && isdigit(nbr[i - 1]));
+}
+
+int	displayJsonElement(char *path, char *key)
+{
+	JsonParserResult	result = JsonParser_parseFile(path, NULL);	//Parse the file
+
+	//Verify if an error occured
+	if (result.error) {
+		printf("%s: %s\n", path, result.error);
+		return EXIT_FAILURE;
+	}
+
+	//Display the file if no key was given
+	if (!key) {
+		JsonParser_printElement(result.data, result.type, NULL);
+		return EXIT_SUCCESS;
+	}
+
+	//Verify the content type
+	if (result.type == JsonParserObjType) {
+		JsonParserObj	*object = JsonParserObj_getElement(result.data, key);
+
+		//Verify if this object has the key
+		if (!object) {
+			printf("%s: The key \"%s\" doesn't exist in this file", path, key);
+			return EXIT_FAILURE;
+		} else
+			JsonParser_printElement(object->data, object->type, NULL);
+	} else if (result.type == JsonParserListType && strIsNbr(key)) {
+		JsonParserList	*list = JsonParserList_getElement(result.data, atoi(key));
+
+		if (!list) {
+			printf("%s: The key \"%s\" doesn't exist in this file", path, key);
+			return EXIT_FAILURE;
+		} else
+			JsonParser_printElement(list->data, list->type, NULL);
+	} else if (result.type == JsonParserListType) {
+		printf("%s: Only integer keys are valid for arrays", path);
+	} else {
+		printf("%s: Only array and objects can have a key", path);
+	}
+	return EXIT_SUCCESS;
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc <= 1 || argc >= 4) {
+		printf("Usage: %s <file.json> <key>\n", argv[0]);
+		return EXIT_SUCCESS;
+	}
+	return (displayJsonElement(argv[1], argc == 3 ? argv[2] : NULL));
+}
